4272693305d7bf77bef6ac2eb7051c649b14b481
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-01-25 21:13:26 +0000

    Create user

--- script.ts
+++ script.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 async function main() {
+  // Create user
   const user = await prisma.user.create({
     data: {
       name: "Alice",
